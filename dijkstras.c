7ade44dfbe07cdfc306474d7cdebf9132b41ff7e
nehaneha27 <98949585+nehaneha27@users.noreply.github.com>
2022-05-10 19:47:26 +0530

    Add files via upload

---
+++ dijkstras.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#define INFINITY 9999
+#define MAX 10
+void dijkstra(int G[MAX][MAX],int n,int startnode);
+void main()
+{int G[MAX][MAX],i,j,n,u;
+
+printf("\nEnter number od nodes:");
+scanf("%d",&n);
+printf("\n enter adjacency matrix:");
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+scanf("%d",&G[i][j]);
+printf("\n enter the starting node:");
+scanf("%d",&u);
+dijkstra(G,n,u);
+
+}
+void dijkstra(int G[MAX][MAX],int n,int startnode)
+{int cost[MAX],distance[MAX],pred[MAX];
+int visited[MAX],count,mindistance,nextnode,i,j;
+
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+if(G[i][j]==0)
+cost[i][j]=INFINITY;
+else
+cost[i][j]=G[i][j];
+
+for(i=0;i<n;i++)
+{distance[i]=cost[startnode][i];
+pred[i]=startnode;visited[i]=0;
+}
+distance[startnode]=0;visited[startnode]=1;
+count=1;
+while(count<n-1)
+{mindistance=INFINITY;
+for(i=0;i<n;i++)
+if(distance[i]<mindistance &&!visited[i])
+{mindistance=distance[i];
+nextnode=i;
+}
+visited[nextnode]=1;
+for(i=0;i<n;i++)
+if(!visited[i])
+if(mindistance+cost[nextnode][i]<distance[i]);
+{distance[i]=mindistance+cost[nextnode][i];
+pred[i]=nextnode;
+}
+count++;
+}
+
+for(i=0;i<n;i++)
+if(i!=startnode)
+{printf("\ndistance of %d=%d",i,distance[i]);
+printf("\n path=%d",i);
+j=i;
+do{
+j=pred[i];
+printf("<-%d",j);
+}while(j!=startnode);
+}
+}
+
+
