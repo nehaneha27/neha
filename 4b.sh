beda1c1dea83b065fb289c36b33a086014339161
nehaneha27 <98949585+nehaneha27@users.noreply.github.com>
2022-05-16 16:29:43 +0530

    Add files via upload

--- 4b.sh
+++ 4b.sh
@@ -1,34 +1,14 @@
-echo "enter a password"
-read pass1
-stty -echo
-echo " confirm password"
-stty -echo
-read pass2
+if [ $# != " " ]
+then
+if [ -f $1 ]
+then
+cd `pwd`
+link=`ls -l $1 | tr -s " " | cut -d " " -f 2 | head -c 3`
 
-while [ "$pass1" != "$pass2" ]
-do
-clear
-echo " -n password does not match"
-echo " please re-enter the password to lock"
-stty echo
-read pass2
-stty -echo
-done
-clear
-echo "----------- Terminal Locked -------------"
-echo "========================================="
+else
+cd ..
 
-echo "enter the password to unlock "
-read pass3
-stty echo
-while [ "$pass1" != "$pass3" ]
-do
-
-echo "----- Terminal locked --------"
-echo " password is incorrect "
-echo " please enter password to unlock "
-read pass3
-stty echo
-done
-echo "---------- Unlocked -----------"
-
+link=`ls -l $1 | tr -s " " | cut -d " " -f 2 | head -c 3`
+fi
+fi
+echo $link
