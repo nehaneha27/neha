2369da39ff8eff41bbe9621d471efdc64d6887ee
nehaneha27 <98949585+nehaneha27@users.noreply.github.com>
2022-05-16 14:54:46 +0530

    Add files via upload

--- 9b.sh
+++ 9b.sh
@@ -1,30 +1,31 @@
-echo -n "Enter a filename:"
+echo -n "enter the file name:"
 read filename
 if [ -f $filename ]
-then 
-	n=10
-	while read line
-	do 
-		ll=` expr length "$line" `
-		if [ $ll -le $n ]
-		then
-			echo $line
-		else
-			s=1
-			e=$n
-			echo "`echo $line | cut -c $s-$e`\\"
-			rcl=` expr $ll -$e `
-			while [ $rcl -ge $n ] 
-			do
-				s=` expr $e + 1 `
-				e=` expr $e + $n `
-				echo "`echo $line | cut -c $s-$e`\\"
-				rcl=` expr $ll -$e `
-			done
-			s=` expr $e + 1 `
-			echo $file | cut -c $s-$e
-		fi
-	done < $filename
+then
+n=40
+while read line
+do
+l=`expr length "$line"`
+if [ $l -lt $n ]
+then
+echo $line
 else
-	echo "$filename:No such file found"
-fi 
+s=1
+e=$n
+echo "`echo $line | cut -c $s-$e` \\"
+rcl=`expr $l - $e`
+while [ $rcl -ge $n ]
+do
+s=`expr $e + 1`
+e=`expr $e + $n`
+echo "`echo $line | cut -c $s-$e` \\"
+rcl=`expr $l - $e`
+done
+s=`expr $e + 1`
+echo $line | cut -c $s-
+fi
+done < $filename
+else
+echo "no such file"
+fi
+
