e39768182668c0ce164f3c5d26c9aaffcab35abe
nehaneha27 <98949585+nehaneha27@users.noreply.github.com>
2022-08-06 10:57:37 +0530

    Add files via upload

---
+++ an.js
@@ -0,0 +1,13 @@
+var n=prompt("enter no");
+p=0;
+l=n.length;
+for(i=0;i<l;i++)
+{
+    p+=n[i]**l;
+    
+}
+if(n==p)
+
+alert("anstm");
+else
+alert("not anst");
---
+++ extr.js
@@ -0,0 +1,5 @@
+n=5;
+for(i=1;i<=n;i++)
+{
+    console.log(i);
+}
